1ef116ea431adfb7af9bb4af30185d6a5962c680
Tilavuz <tilav777@gmail.com>
2024-07-31 10:52:13 +0500

    ...

--- src/components/common/department-theme/theme-form.tsx
+++ src/components/common/department-theme/theme-form.tsx
@@ -1,7 +1,7 @@
 import { apiClient } from "@/api/api-client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { addtheme } from "@/features/department-theme/department-theme-slice";
+import { addtheme, changetheme } from "@/features/department-theme/department-theme-slice";
 import { FormEvent, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { toast } from "sonner";
@@ -9,9 +9,13 @@ import { toast } from "sonner";
 export default function ThemeForm({
   subjectId,
   departmentId,
+  title,
+  id
 }: {
   subjectId: string;
   departmentId: string;
+  title?: string;
+  id?: string
 }) {
   const titleRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
@@ -24,17 +28,35 @@ export default function ThemeForm({
         departmentId,
         title: titleRef?.current?.value,
       };
-      const res = await apiClient.post("/themes/add", themeData);
-      dispatch(addtheme({theme: res.data.theme, id: departmentId}));
-      toast.success(res.data.message);
+
+      if (!title) {
+        const res = await apiClient.post("/themes/add", themeData);
+        dispatch(addtheme({ theme: res.data.theme, id: departmentId }));
+        toast.success(res.data.message);
+      }
+
+      if (title && id) {
+        const res = await apiClient.put(`/themes/update/${id}`, themeData);
+        dispatch(changetheme({ theme: res.data.theme, id: departmentId }));
+        toast.success(res.data.message);
+      }
     } catch (error) {
       const result = error as Error;
       toast.error(result.message);
     }
   };
 
-  return <form onSubmit={(e) => createTheme(e)} className="p-2">
-    <Input type="text" ref={titleRef} placeholder="Mavzuni kiriting!" className="mb-2" />
-    <Button>Kiritish</Button>
-  </form>;
+  return (
+    <form onSubmit={(e) => createTheme(e)} className="p-2">
+      <Input
+        required
+        defaultValue={title ?? ""}
+        type="text"
+        ref={titleRef}
+        placeholder="Mavzuni kiriting!"
+        className="mb-2"
+      />
+      <Button>Kiritish</Button>
+    </form>
+  );
 }
--- src/components/common/department/department-body.tsx
+++ src/components/common/department/department-body.tsx
@@ -21,7 +21,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { getThemes } from "@/features/department-theme/department-theme-slice";
+import { getThemes, removetheme } from "@/features/department-theme/department-theme-slice";
 import {
   getDepartments,
   removeDepartment,
@@ -32,6 +32,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 import ThemeForm from "../department-theme/theme-form";
+import DepartmentForm from "./department-form";
 
 export default function DepartmentBody() {
   const { id } = useParams();
@@ -78,6 +79,17 @@ export default function DepartmentBody() {
     }
   };
 
+  const deleteTheme = async (id: string) => {
+    try {
+      const res = await apiClient.delete(`/themes/delete/${id}`);
+      toast.success(res.data.message);
+      dispatch(removetheme(id));
+    } catch (error) {
+      const result = error as Error;
+      toast.error(result.message);
+    }
+  };
+
   return (
     <div className="">
       {departments &&
@@ -105,6 +117,24 @@ export default function DepartmentBody() {
                       >
                         Delete
                       </ContextMenuItem>
+
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <button className="outline-none text-sm px-2 py-[6px] hover:bg-[#f5f7fa] w-full text-left rounded">
+                            Edit
+                          </button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle></DialogTitle>
+                            <DialogDescription></DialogDescription>
+                          </DialogHeader>
+                          <DepartmentForm
+                            id={department._id}
+                            title={department.title}
+                          />
+                        </DialogContent>
+                      </Dialog>
                     </ContextMenuContent>
                   </ContextMenu>
                   <Dialog>
@@ -132,7 +162,38 @@ export default function DepartmentBody() {
                         .filter((theme) => theme.id === department._id)
                         .map((item, i) => (
                           <li key={item.theme._id}>
-                            {i+1}) {item.theme.title}
+                            <ContextMenu>
+                              <ContextMenuTrigger>
+                                {i + 1}) {item?.theme?.title}
+                              </ContextMenuTrigger>
+                              <ContextMenuContent>
+                                <ContextMenuItem
+                                  onClick={() => deleteTheme(item.theme._id)}
+                                  className="text-red-500"
+                                >
+                                  Delete
+                                </ContextMenuItem>
+                                <Dialog>
+                                  <DialogTrigger asChild>
+                                    <button className="outline-none text-sm px-2 py-[6px] hover:bg-[#f5f7fa] w-full text-left rounded">
+                                      Edit
+                                    </button>
+                                  </DialogTrigger>
+                                  <DialogContent>
+                                    <DialogHeader>
+                                      <DialogTitle></DialogTitle>
+                                      <DialogDescription></DialogDescription>
+                                    </DialogHeader>
+                                    <ThemeForm
+                                      departmentId={department._id}
+                                      subjectId={id!}
+                                      title={item.theme.title}
+                                      id={item.theme._id}
+                                    />
+                                  </DialogContent>
+                                </Dialog>
+                              </ContextMenuContent>
+                            </ContextMenu>
                           </li>
                         ))}
                   </ul>
